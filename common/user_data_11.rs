a452cc7e86eb65e7c1785fc015b43d243c4d1d66
groobybugs <groobybugs299@hotmail.com>
2024-09-19 00:18:04 -0600

    Update ps4 memory offsets

--- common/user_data_11.rs
+++ common/user_data_11.rs
@@ -13,8 +13,8 @@ impl Default for UserData11 {
     fn default() -> Self {
         Self { 
             unk: Default::default(), 
-            regulation: vec![0; 0x1ee6c0],
-            rest: vec![0;0x51940]
+            regulation: vec![0; 0x1F0DE0],
+            rest: vec![0;0x4F220]
         }
     }
 }
@@ -22,10 +22,9 @@ impl Default for UserData11 {
 impl Read for UserData11 {
     fn read(br: &mut BinaryReader) -> Result<UserData11, Error> {
         let mut user_data_11 = UserData11::default();
-        //br.jump(0x1960080);
         user_data_11.unk.copy_from_slice(br.read_bytes(0x10)?);
-        user_data_11.regulation.copy_from_slice(br.read_bytes(0x1ee6c0)?);
-        user_data_11.rest.copy_from_slice(br.read_bytes(0x51940)?);
+        user_data_11.regulation.copy_from_slice(br.read_bytes(0x1F0DE0)?);
+        user_data_11.rest.copy_from_slice(br.read_bytes(0x4F220)?);
         assert_eq!(user_data_11.rest[0], 0);
         Ok(user_data_11)
     }
